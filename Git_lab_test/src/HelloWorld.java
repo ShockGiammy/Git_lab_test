64529a65dc818d7bd306189c53750af723d81369
Gian Marco Falcone <gian.marco.1698@gmail.com>
2021-03-12 12:27:47 +0100

    println changed with a logger

--- Git_lab_test/src/HelloWorld.java
+++ Git_lab_test/src/HelloWorld.java
@@ -1,8 +1,12 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
  
-       System.out.println("Hello, World!");
+       Logger logger = Logger.getLogger(HelloWorld.class.getName());
+       logger.log(Level.INFO, "Hello, World!");
  
     }
  }
