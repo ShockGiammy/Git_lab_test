88617913837256e0bdaa565b6d1305b7b6f4423e
Gian Marco Falcone <gian.marco.1698@gmail.com>
2021-03-25 23:06:55 +0100

    added Maven and general refactoring

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,12 @@
+package main.java;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-    	
+    	  	
     	Logger logger = createLogger();
     	logger.log(Level.INFO, "Hello, World!");    
     }
